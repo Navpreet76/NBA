8ff86bcea844b02005c0455afa8dd248a581c76b
Navpreet Chhina <nchhina76@gmail.com>
2017-08-08 18:29:12 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include 'header.php' ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Nba Home page</title>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+</head>
+<body>
+<div id="wrapper">
+<div id="banner"><img src="images/nba1.jpg" alt="nba1" width="90%"></div>
+	
+
+
+
+
+
+
+
+
+</div>
+
+</body>
+</html>
